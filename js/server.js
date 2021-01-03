d8e7af03b42647051387f8d7c63e2af94947ea0a
chaos <liuchao46642@yahoo.co.jp>
2021-01-03 18:30:56 +0900

    add server

---
+++ js/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+function start(){
+  const html = require('fs').readFileSync('src/index.html')
+  const server = http.createServer(function(request,response){
+    response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+    process.on('uncaughtException', (e) => console.log(e));
+
+    response.end(html);
+  })
+
+
+  server.listen(3000,'127.0.0.1');
+  console.log("Server has started");
+}
+
+exports.start = start;
--- js/skill_tree.js
+++ js/skill_tree.js
@@ -1,3 +1,7 @@
+const server = require("./server");
+
+server.start();
+
 function sum(a, b) {
   return a + b;
 }
